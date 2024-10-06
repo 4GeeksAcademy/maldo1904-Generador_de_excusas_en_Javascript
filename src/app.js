f99b33f56db06c636fe9cb64b138a15f85715c1c
José Daniel Maldonado Flores <danielmaldo_31@hotmail.com>
2024-10-06 21:23:15 +0000

    termine

--- src/app.js
+++ src/app.js
@@ -8,4 +8,33 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  //declaro mis datos
+  //           0              1            2             3        INDICE
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function Excusa() {
+    //Logica   Numero entero (redondeado a un numero menor). Numero alaeatorio. Tamaño especifico
+    let quien = Math.floor(Math.random() * who.length);
+    let accion = Math.floor(Math.random() * action.length);
+    let que = Math.floor(Math.random() * what.length);
+    let cuando = Math.floor(Math.random() * when.length);
+
+    //Lo que regresa
+    return (
+      who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
+    );
+  }
+
+  // Establezco la excusa generada en el elemento con el id #excuse
+  document.querySelector("#excuse").innerHTML = Excusa();
 };

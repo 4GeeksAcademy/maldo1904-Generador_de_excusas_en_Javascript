92ca1787a6d0a3986b3fc9b2372eb13d0719d14a
José Daniel Maldonado Flores <danielmaldo_31@hotmail.com>
2024-10-17 03:11:44 +0000

    CORECCION

--- src/app.js
+++ src/app.js
@@ -1,40 +1,38 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let quien_sujeto = ["The dog", "My grandma", "The mailman", "My bird"];
+let actividad = ["ate", "peed", "crushed", "broke"];
+let que_cosa = ["my homework", "my phone", "the car"];
+let cuando_se_hizo = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function Excusa() {
+  let quien = Math.floor(Math.random() * quien_sujeto.length);
+  let accion = Math.floor(Math.random() * actividad.length);
+  let que = Math.floor(Math.random() * que_cosa.length);
+  let cuando = Math.floor(Math.random() * cuando_se_hizo.length);
+
+  return (
+    quien_sujeto[quien] +
+    " " +
+    actividad[accion] +
+    " " +
+    que_cosa[que] +
+    " " +
+    cuando_se_hizo[cuando]
+  );
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-
-  //declaro mis datos
-  //           0              1            2             3        INDICE
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
-
-  function Excusa() {
-    //Logica   Numero entero (redondeado a un numero menor). Numero alaeatorio. Tamaño especifico
-    let quien = Math.floor(Math.random() * who.length);
-    let accion = Math.floor(Math.random() * action.length);
-    let que = Math.floor(Math.random() * what.length);
-    let cuando = Math.floor(Math.random() * when.length);
-
-    //Lo que regresa
-    return (
-      who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
-    );
-  }
-
-  // Establezco la excusa generada en el elemento con el id #excuse
   document.querySelector("#excuse").innerHTML = Excusa();
+  const boton = document.querySelector(".btn-generar");
+  boton.addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = Excusa();
+  });
 };
